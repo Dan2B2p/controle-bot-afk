3d5b1035ec46da61f3f3b4935c770c2173b39c97
Dan2B2p <itsagotimezixcs@gmail.com>
2025-01-11 21:17:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const mineflayer = require('mineflayer');
 const app = express();
 const port = 3000;
 
-// Variável para controlar o estado do bot
 let bot;
 let botStarted = false;
 
-// Configurações do servidor
-app.use(express.static('public')); // Para servir o HTML
+app.use(express.static('public')); // Serve arquivos estáticos da pasta 'public'
 
 // Rota para controlar o estado do bot
 app.get('/toggle-bot', (req, res) => {
+  console.log('Recebido pedido para alternar o bot...'); // Log para verificar se a rota está sendo chamada
   if (botStarted) {
+    console.log('Bot já está iniciado, desconectando...');
     bot.quit(); // Desconecta o bot
     botStarted = false;
     res.json({ status: 'stopped' });
   } else {
-    // Cria o bot
+    console.log('Iniciando o bot...');
     bot = mineflayer.createBot({
-      host: 'survival_b1p.aternos.me',
-      port: 14892,
-      username: 'AFK_B1p',
+      host: 'survival_b1p.aternos.me', // IP do servidor
+      port: 14892, // Porta do servidor
+      username: 'AFK_B1p', // Nome do bot
     });
 
     bot.on('login', () => {
